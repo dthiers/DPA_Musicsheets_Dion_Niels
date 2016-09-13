b222c292ce1f006467ab1cdf49b753f70c87a7ab
D Thiers <dthiers@student.avans.nl>
2016-09-13 11:44:19 +0200

    Project setup.

---
+++ DPA_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using DPA_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note = new Note();
+
+            Console.WriteLine(note.call_self());
+
+            Console.ReadKey();
+        }
+    }
+}
