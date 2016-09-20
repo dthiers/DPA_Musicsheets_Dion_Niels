3688aa54f9615ac6e4e3400b916ad140a8de97fb
D Thiers <dthiers@student.avans.nl>
2016-09-20 13:50:50 +0200

    Added some enums

---
+++ DPA_Core/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Core
+{
+    class Note
+    {
+        // C, D, E, F, G, A, B
+        String pitch { get; set; }
+
+        /*
+         * In most cases, a sharp raises the pitch of a note one semitone while a flat lowers it a semitone. 
+         * A natural is used to cancel the effect of a flat or sharp. 
+         * */
+        String accidental { get; set; }
+
+        // Geen idee?
+        String timeSignature { get; set; }
+
+
+    }
+}
---
+++ DPA_Core/Pitch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Core
+{
+    public enum Pitch {
+        C,
+        D,
+        E,
+        F,
+        G,
+        A,
+        B
+    }
+}
